3d7eb1bbc33ed2085801dc9f94a788d609cf3c1a
AndreiPatapovich <andreivolk1985@gmail.com>
2023-03-07 22:06:47 +0300

    multiplicity

--- lesson_15.2.2.py
+++ lesson_15.2.2.py
@@ -1,24 +1,20 @@
 nums_list = []
 
-N = int(input('Кол-во чисел в списке: '))
+amount_of_numbers = int(input('Кол-во чисел в списке: '))
 
-for _ in range(N):
-    num = int(input('Очередное число: '))
+for number in range(amount_of_numbers):
+    print('Введите', number + 1, end = '')
+    num = int(input(' число: '))
 
     nums_list.append(num)
 
-maximum = nums_list[0]
-
-minimum = nums_list[0]
-print(nums_list)
+k_dell = int(input('Введите делитель: '))
+count_ind = -1
+summ = 0
 for i in nums_list:
+    count_ind += 1
+    if i % k_dell == 0:
+        summ += count_ind
+        print('Индекс числа', i ,':', count_ind)
 
-    if maximum < i:
-        maximum = i
-
-    if minimum > i:
-        minimum = i
-
-print('Максимальное число в списке:', maximum)
-
-print('Минимальное число в списке:', minimum)
+print('Сумма индексов: ', summ)

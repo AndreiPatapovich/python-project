f04dbaa518a41e086b59d72c9e87f831549a0098
AndreiPatapovich <andreivolk1985@gmail.com>
2023-03-17 20:26:07 +0300

    umber_of_employees

--- lesson_3.2.1.py
+++ lesson_3.2.1.py
@@ -1,8 +1,19 @@
-zoo = ['lion', 'kangaroo', 'elephant', 'monkey']
-print(zoo)
-zoo.insert(1, 'bear')
-print(zoo)
-zoo.remove('elephant')
-print(zoo)
-print('Лев сидит в клетке номер ', zoo.index('lion') + 1)
-print('Обезьяна сидит в клетке номер ', zoo.index('monkey') + 1)
+umber_of_employees = int(input('Кол-во сотрудников: '))
+salaries_list = []
+# print(f'Количество сотрудников: {umber_of_employees}')
+for i in range(umber_of_employees):
+    print('Зарплата', i + 1, 'сотрудника', end = ': ')
+    salary = int(input(''))
+    salaries_list.append(salary)
+
+print(salaries_list)
+
+for ind in salaries_list:
+    if ind == 0:
+        salaries_list.remove(ind)
+        umber_of_employees -= 1
+print(f'зарплаты: {salaries_list}')
+print(f'Осталось сотрудников: {umber_of_employees} .')
+
+print(f'Максимальная зарплата сотрудников: {max(salaries_list)}')
+print(f'Минимальная зарплата сотрудников: {min(salaries_list)}')

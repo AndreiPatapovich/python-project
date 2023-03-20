4815bfbe771676e3337fb8bdd4511057194e64e5
AndreiPatapovich <andreivolk1985@gmail.com>
2023-03-20 19:38:13 +0300

    message

--- lesson_16.3.3.py
+++ lesson_16.3.3.py
@@ -1,13 +1,9 @@
-main = [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1]
 
-first_company = [0, 0, 0]
+first_word = input("Первое сообщение: ")
+second_word = input("Первое сообщение: ")
+first_count = first_word.count("!") + first_word.count("?")
+second_count = second_word.count("!") + second_word.count("?")
+if first_count < second_count:
+    first_word, second_word = second_word, first_word  # пусть первым словом будет то, в котором больше знаков
 
-second_company = [1, 0, 0, 1, 1]
-
-third_company = [1, 1, 1, 0, 1]
-
-main.extend(first_company + second_company + third_company)
-
-a = main.count(0)
-print(f'Общий список задач: {main}')
-print(f'Кол-во невыполненных задач: {a}')
+print(first_word + second_word)

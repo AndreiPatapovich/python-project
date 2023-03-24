a2b99cd113823225282b0013d9b2eb69f97be599
AndreiPatapovich <andreivolk1985@gmail.com>
2023-03-24 18:13:19 +0300

    commands

--- lesson_16.3.4.py
+++ lesson_16.3.4.py
@@ -1,11 +1,13 @@
+quantity_sport = int(input('Сколько человек: '))
+commands = int(input('Кол-во человек в команде: '))
 
-def matrix_num(matrix):
+sport_list = []
+N = 1
+if quantity_sport % commands == 0:
+  for i in range(quantity_sport // commands):
+      sport_list.append(list(range(N, N + commands)))
+      N += commands
+  print(f'Общий список комманд: {sport_list}')
+else:
+    print(f'{quantity_sport} участников нельзя раделить на команды по {commands} человек(а).')
 
-    for i_list in matrix:
-        for i_num in i_list:
-            print(i_num, end = ' ')
-        print()
-
-m = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-matrix_num(m)
